13bdcb3de7ec5d60d5f53b4fc824393b7512f36d
Juan López <jlopez@finboot.com>
2022-03-12 12:18:08 +0100

    Add menu back to wallet page

--- src/minter_assets/Wallet.tsx
+++ src/minter_assets/Wallet.tsx
@@ -1,8 +1,33 @@
 import React from "react"
+import {Link} from "react-router-dom";
 
 export default function Wallet() {
   return (
     <>
+      <div>
+        <h2>Menu</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link">Home</Link></li>
+          </ul>
+        </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/wallet'} className="nav-link">Wallet</Link></li>
+          </ul>
+        </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/minter'} className="nav-link">Minter</Link></li>
+          </ul>
+        </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/my-nfts'} className="nav-link">MyNFTs</Link></li>
+          </ul>
+        </nav>
+        <hr />
+      </div>
       <header className="wallet-header">
         <p style={{ fontSize: "2em", marginBottom: "0.5em" }}>Wallet Page</p>
         <div style={{
